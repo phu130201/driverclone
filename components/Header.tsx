c3c2d4986ed81b2bd4984187abea7bdde1066c10
phu <nphu9500@gmail.com>
2024-12-21 09:19:06 -0800

    firstCommit

---
+++ components/Header.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import FileUploader from "@/components/FileUploader";
+import { signOutUser } from "@/lib/action/user.action";
+
+const Header = ({
+  userId,
+  accountId
+}: {
+  userId: string;
+  accountId: string;
+}) => {
+  return (
+    <header className="header">
+      <div className="header-wrapper">
+        <FileUploader ownerId={userId} accountId={accountId} />
+        <form
+          action={async () => {
+            "use server";
+
+            await signOutUser();
+          }}>
+          <Button type="submit" className="sign-out-button">
+            <Image
+              src="/assets/icons/logout.svg"
+              alt="logo"
+              width={24}
+              height={24}
+              className="w-6"
+            />
+          </Button>
+        </form>
+      </div>
+    </header>
+  );
+};
+export default Header;
